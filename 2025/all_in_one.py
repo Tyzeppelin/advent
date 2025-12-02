52ca55c1737f0ba2279905402497fc313fa76132
François Boschet <boschet.francois@gmail.com>
2025-12-02 17:28:33 +0100

    day two, a bit late

--- 2025/all_in_one.py
+++ 2025/all_in_one.py
@@ -23,12 +23,28 @@ def day_one():
 
     return password_1, password_2
 
+def day_two():
+    # plan:
+    # log10(up) => size repeatable = log10 / 2
+    # up[:rep] - low[:rep]
+    with open("day_two.txt", 'r') as f:
+        for low, up in [r.split("-") for r in f.read().rstrip().split(",")]:
+            #print(up - low, "\t", low, up,)
+            rep_size = int(math.log10(int(low))) + 1 # need to account for the decimal part
+            res = int(up[:rep_size]) - int(low[:rep_size])
+            print(res)
+    return "NOT YET"
+
 
 if __name__ == "__main__":
 
     t1 = time.time()
 
-    print("Day one - part 1")
+    print("Day One - part 1 & 2")
     print("=>", day_one())
+    print("********************")
+    print("Day Two - part 1")
+    print("=>", day_two())
+    print("********************")
 
     print("done under", time.time() - t1, "seconds")
