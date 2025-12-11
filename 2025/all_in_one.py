15d9db4dc2b5196b305df3c8d200d5b547cde56f
François Boschet <boschet.francois@gmail.com>
2025-12-11 17:21:40 +0100

    wip

--- 2025/all_in_one.py
+++ 2025/all_in_one.py
@@ -5,6 +5,7 @@ import math
 import operator
 import time
 
+_primes = [2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 41, 43, 47]
 
 def day_one():
     dial = 50
@@ -23,6 +24,10 @@ def day_one():
 
 
 def _npeat(n, l):
+    """
+    n -> how many time
+    l -> length of the number
+    """
     return sum([10**x for x in range(0, l*n, l)])
 
 
@@ -36,29 +41,57 @@ def sum_npeat(a, b, n=2):
     return npeat_factor * (b - a + 1) * (a + b) / 2
     
 
-#def _8peat(n):
-#    return (10**n-1)*8/9
+def set_npeat(a, b, n=2):
+    assert math.floor(math.log10(a)) == math.floor(math.log10(b)), f"a:{a} and b:{b} must be the same size"
+    l_a = math.floor(math.log10(a)) + 1
+    npeat_factor = _npeat(n, l_a)
+    return {npeat_factor * m for m in range(a, b+1)}
+
+def rect_range(i, j, sz):
+    # min
+    a = int(i) if len(i) == sz else 10**(sz-1)
+    # max
+    b = int(j) if len(j) == sz else 10**sz - 1
+    return a,b
+
 
 def day_two():
     # plan:
     part_1 = 0
-
+    part_2 = 0
     with open("day_two.txt", 'r') as f:
-        for l, u in [r.split("-") for r in f.read().rstrip().split(",")]:
+        for l, u in [r.split("-") for r in f.read().rstrip().split(",")][:5]:
             low = int(l)
             up = int(u)
             l_low = len(l)
             l_up = len(u)
             low_bound = math.ceil(l_low / 2)
             up_bound = l_up // 2 + 1
+            part1_range = list(range(low_bound, up_bound, 1))
             # Part 1: we get the range of the size of the len 
-            for q in range(low_bound, up_bound, 1):
-                # min
-                a = 10 ** (q-1) if 2*q != l_low else int(l[:q]) if int(l[:q]) >= int(l[q:]) else int(l[:q]) + 1
-                # max
-                b = 10 ** (q) - 1 if 2*q != l_up else int(u[:q]) if int(u[:q]) <= int(u[q:]) else int(u[:q]) - 1
-                s2 = sum_npeat(a, b, 2)
-                part_1 += s2
+            # Part 2: we use a set because I am not smart enough
+            invalid_set = set()
+            print("$", low, up)
+            for sz in range(l_low, l_up+1):
+                print("------------------", sz)
+                invalid_set |= {int("1"*sz)}
+                print(sz, [1] + [e for e in range(1, up_bound, 1) if e in _primes and sz % e == 0])
+                for q in [1] + [e for e in range(1, up_bound, 1) if e in _primes and sz % e == 0]:
+                    #print("q:", q, invalid_set)
+                    # min
+                    a = 10 ** (q-1) if 2*q != l_low else int(l[:q]) if int(l[:q]) >= int(l[q:]) else int(l[:q]) + 1
+                    # max
+                    b = 10 ** (q) - 1 if 2*q != l_up else int(u[:q]) if int(u[:q]) <= int(u[q:]) else int(u[:q]) - 1
+                    #print("=", l, u, a, b)
+                    if sz / q == 2.0:
+                        #print("here")
+                        s2 = sum_npeat(a, b, 2)
+                        part_1 += s2
+                    invalid_set = invalid_set | set_npeat(_nearest(a, q), _nearest(b, q), q)
+            #print(l, u, sorted(invalid_set))
+            else:
+                print("=======================")
+
 
     return part_1, "NOT YET"
 
